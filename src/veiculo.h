cac532be96c723f14c1680cde31656546fc16395
Silas Vinícius <svestudos40@gmail.com>
2026-06-11 13:15:53 -0300

    veiculo.h V1.0

---
+++ src/veiculo.h
@@ -0,0 +1,9 @@
+#ifndef VEICULO_H
+#define VEICULO_H
+
+void cadastrar_veiculo(void);
+void listar_veiculos(const char *filtro_status);
+void atualizar_status_veiculo(void);
+void menu_veiculos(void);
+
+#endif /* VEICULO_H */
